75e22d681109fcb1216a06881c706011f011c144
josolha <josolha@nate.com>
2022-12-21 23:11:08 +0900

    front study

---
+++ JAVASCRIPT/Project06/js/ex06.js
@@ -0,0 +1,14 @@
+/**
+ *  ex06.js파일로 챕터 6의 js 파일임
+	auth : kook
+	date : 2022.12.05
+ */
+ 
+function over(obj){ 
+	// function은 js에서 함수선언 키워드, 함수 이름(파라미터 이름)으로 자바 처럼
+	//리턴타입, 데이터 타입이 없음 
+	obj.src = "media/banana.png";
+}
+function out(obj){
+	obj.src = "media/apple.png";
+}
